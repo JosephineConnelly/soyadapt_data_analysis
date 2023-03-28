9c0606312631de2f8e6726258ea23855519176a2
Josephine E A Connelly <josephineconnelly@qgg.au.dk>
2023-03-28 10:24:54 +0200

    march 28

--- workflow/scripts/filter_quality.sh
+++ workflow/scripts/filter_quality.sh
@@ -9,21 +9,32 @@
 #SBATCH --time=06:00:00                               # Time limit hrs:min:sec
 #SBATCH --output=serial_output_%j.out                  # Standard output and error log
 
-# here i am trying the quality filter (qf) for the cca_sbpa
 
-conda activate soy_data_setup
+# Initialize conda with either of the following options:
+
+# 1. Source your .bashrc
+
+#source /wsu/zz/zz99/zz9992/.bashrc
+
+# Activate your conda env
+
+#conda activate myenv 
+
+
+source /home/joco/miniconda3/etc/profile.d/conda.sh
+source activate soy_data_setup
+ # here i am trying the quality filter (qf) for the cca_sbpa
 cd /home/joco/faststorage/data_analysis_soy
-pwd
 
 #quality filter qf
-# specify the input VCF file name
+# specify the input VCF file name cca_sbpa_Gm_only.vcf.gz
 # remove MAF < 1% 
 # remove SNPs with >20% missing calls
 # remove sites with >10% heterozygosity and monomorphic SNPs
 # remove sites with missing data > 5%
 # remove non-biallelic sites
 # specify the output filetype -Oz (.vcf.gz)
-# specify the output VCF file name
+# specify the output VCF file name and destination
 
 bcftools filter cca_sbpa_Gm_only.vcf.gz \
 -e 'INFO/AF < 0.01' \ 
@@ -33,4 +44,7 @@ bcftools filter cca_sbpa_Gm_only.vcf.gz \
 -e 'N_ALLELES != 2' \ 
 -Oz -o filtered/cca_sbpa_qf.vcf.gz 
 
+# index the filtered file
 bcftools index cca_sbpa_qf.vcf.gz
+#count snps in the filtered file there were 9677024 snps in the original file
+bcftools view --no-header -G -m 2 -M 2 --types snps cca_sbpa_qf.vcf.gz | wc -l
